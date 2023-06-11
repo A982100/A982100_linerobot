934ab5099dc18f9d0c9fd8f04339b48703b3e95d
A982100 <cc7867863@gmail.com>
2023-06-11 19:45:50 +0800

    init

---
+++ A982100_linerobot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A982100_linerobot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
